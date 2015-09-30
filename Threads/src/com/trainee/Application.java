34dc5ee285581e30f3e358ec88f2bc3cda183f2d
csha007 <csha10@noilab66147.sapient.com>
2015-09-30 12:17:31 +0530

    Firt Commit

---
+++ Threads/src/com/trainee/Application.java
@@ -0,0 +1,15 @@
+package com.trainee;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		new UsePrintString("Idly","Sambhar");
+		new UsePrintString("Parantha","Chutney");
+		new UsePrintString("Pizza","Coke");
+		new UsePrintString("Roti","Daal");
+		
+	}
+
+}
